4d9eb79a40d80c76c37483869cf9949c603169f1
ji-mmyliu <jimmyjcl753@gmail.com>
2022-01-04 16:09:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pywebio
-from pywebio.input import input, FLOAT, file_upload, textarea
+from pywebio.input import input, FLOAT, file_upload, textarea, select
 from pywebio.output import put_text, put_html, put_markdown, put_table, put_file, scroll_to, put_button
 from pywebio.session import set_env
 import pymongo
@@ -52,124 +52,109 @@ def private_problems():
         put_markdown("Sorry, the password you entered was incorrect.")
         scroll_to(position = "bottom")
 
+def lang():
+    data = [["Language", "Compilation", "Execution"]]
+    g = settings.find({"type":"lang"})
+    for x in g:
+        lg = [x['name']]
+        lg.append(x['compl'].format(x = 0, path="path") if len(x['compl']) > 0 else "not a compiled language")
+        lg.append(x['run'].format(x = 0, t = 0, path="path"))
+        data.append(lg)
+    put_markdown("## Exact compilation and execution commands for all languages")
+    put_table(data)
+
+def info():
+    put_markdown(open("problem_setting.md", "r").read())
+
+def contest():
+    put_markdown("## Setting up a contest")
+    if not enterPassword():
+        return
+
+    name = input("Enter the contest name:")
+
+    prev = settings.find_one({"type":"contest", "name":name})
+    if not prev is None:
+        put_text("An existing contest with the name \"" + name + "\" was found. If you would like to edit or delete this contest, please contact me.")
+        return
+
+    start = input("Enter the contest start time in the format YYYY MM DD HH MM SS (24-hour time):\n")
+    end = input("Enter the contest end time in the format YYYY MM DD HH MM SS (24-hour time):\n")
+    problems = int(input("Enter the number of problems in the contest:", type=FLOAT))
+    ll = int(input("How long should the participant window be (in seconds): ", type=FLOAT))
+
+    inst = textarea("Paste the contest instructions here (will be shown as a user starts a contest)")
+    with open("instructions.txt", "w") as f:
+        f.write(inst)
+        f.flush()
+        f.close()
+    
+    stc = storage.Client()
+    bucket = stc.get_bucket("discord-bot-oj-file-storage")
+    blob = bucket.blob("ContestInstructions/" + name + ".txt")
+    blob.upload_from_filename("instructions.txt")
+
+    settings.insert_one({"type":"contest", "name":name, "start":start, "end":end, "problems":problems, "len":ll})
+
+    put_text("Successfully created contest `" + str(name) + "`! You may now close this page.")
+
+def view_problems():
+    arr = sorted([(x['name'], x['points'], x['types'], x['authors']) for x in settings.find({"type":"problem", "published":True})], key = cmp_to_key(cmpProblem))
+    data = [
+        ['Problem Name', 'Points/Difficulty', 'Problem Types', 'Authors'],
+    ]
+    for x in arr:
+        data.append([x[0], x[1], ", ".join(x[2]), ", ".join(x[3])])
+    put_markdown("## All published problems on the judge:")
+    put_table(data)
+
+    global clicked
+    clicked = False
+
+    put_button("View private problems", onclick = private_problems, outline = True)
+
+def about():
+    put_markdown(open("about.md", "r").read())
+
+def view_problem():
+    name = input("Enter the problem to open:")
+    judge.problemInterface(settings, name, user['name'])
+
+def login():
+    pswd = input("Please enter your account password to login")
+    global user
+    user = settings.find_one({"type":"account", "pswd":pswd.strip()})
+    if user is None:
+        put_text("Could not find an account associated with the given password")
+        return
+    put_markdown("**Logged in as `" + user['name'] + "`**")
+
+def join():
+    put_markdown("Select the contest to join:")
+    op = [x['name'] for x in settings.find({"type":"contest"})]
+    name = select(options = op)
+
+    if not judge.joinContest(settings, name, user['name']):
+        return
+    judge.instructions(name)
+
+def rank():
+    put_markdown("Select the contest to join:")
+    op = [x['name'] for x in settings.find({"type":"contest"})]
+    contest = select(options = op)
+    put_markdown(judge.getScoreboard(settings, contest))
+
+def rem():
+    global user
+    put_markdown("## Time remaining for joined contests:\n" + judge.remaining(settings, user['name']))
+
 def register():
     set_env(title = "DBOJ Online Console")
 
     try:
         put_markdown("# Welcome to the Discord Bot Online Judge administrator console!")
-        put_table([
-            ['Command', 'Function'],
-            ['info', 'Problem and contest setting instructions and template'],
-            ['lang', 'View exact executions for all languages'],
-            ['rank', 'View the rankings of a contest'],
-            ['c', 'Register a new contest or edit an existing contest'],
-            ['s', 'View all problems on the judge'],
-            ['a', 'See the about page']
-        ])
-        op = input("Please type in a command from the command list above:")
         
-        if op.lower() == 'lang':
-            data = [["Language", "Compilation", "Execution"]]
-            g = settings.find({"type":"lang"})
-            for x in g:
-                lg = [x['name']]
-                lg.append(x['compl'].format(x = 0, path="path") if len(x['compl']) > 0 else "not a compiled language")
-                lg.append(x['run'].format(x = 0, t = 0, path="path"))
-                data.append(lg)
-            put_markdown("## Exact compilation and execution commands for all languages")
-            put_table(data)
-        elif op.lower() == 'info':
-            put_markdown(open("problem_setting.md", "r").read())
-        elif op.lower() == 'c':
-            put_markdown("## Setting up a contest")
-            
-            if not enterPassword():
-                return
-
-            name = input("Enter the contest name:")
-
-            prev = settings.find_one({"type":"contest", "name":name})
-            if not prev is None:
-                put_text("An existing contest with the name \"" + name + "\" was found. If you would like to edit or delete this contest, please contact me.")
-                return
-
-            start = input("Enter the contest start time in the format YYYY MM DD HH MM SS (24-hour time):\n")
-            end = input("Enter the contest end time in the format YYYY MM DD HH MM SS (24-hour time):\n")
-            problems = int(input("Enter the number of problems in the contest:", type=FLOAT))
-            ll = int(input("How long should the participant window be (in seconds): ", type=FLOAT))
-
-            inst = textarea("Paste the contest instructions here (will be shown as a user starts a contest)")
-            with open("instructions.txt", "w") as f:
-                f.write(inst)
-                f.flush()
-                f.close()
-            
-            stc = storage.Client()
-            bucket = stc.get_bucket("discord-bot-oj-file-storage")
-            blob = bucket.blob("ContestInstructions/" + name + ".txt")
-            blob.upload_from_filename("instructions.txt")
-
-            settings.insert_one({"type":"contest", "name":name, "start":start, "end":end, "problems":problems, "len":ll})
-
-            put_text("Successfully created contest `" + str(name) + "`! You may now close this page.")
-        elif op.lower() == 's':
-            arr = sorted([(x['name'], x['points'], x['types'], x['authors']) for x in settings.find({"type":"problem", "published":True})], key = cmp_to_key(cmpProblem))
-            data = [
-                ['Problem Name', 'Points/Difficulty', 'Problem Types', 'Authors'],
-            ]
-            for x in arr:
-                data.append([x[0], x[1], ", ".join(x[2]), ", ".join(x[3])])
-            put_markdown("## All published problems on the judge:")
-            put_table(data)
-
-            global clicked
-            clicked = False
-
-            put_button("View private problems", onclick = private_problems, outline = True)
-        elif op.lower() == 'u':
-            f = file_upload("Upload a file")
-            open('asset/'+f['filename'], 'wb').write(f['content'])  
-        elif op.lower() == 'a':
-            put_markdown(open("about.md", "r").read())
-        elif op.lower().startswith("join"):
-            arr = op.split(" ")
-            if len(arr) < 2:
-                put_text("Use join followed by the contest name")
-                return
-            pswd = input("Enter your account password:")
-            user = settings.find_one({"type":"account", "pswd":pswd.strip()})
-            if user is None:
-                put_text("Could not find an account associated with the given password")
-                return
-            put_markdown("**Logged in as `" + user['name'] + "`**")
-            if not judge.joinContest(settings, arr[1], user['name']):
-                return
-            judge.instructions(arr[1])
-        elif op.lower().startswith("open"):
-            arr = op.split(" ")
-            if len(arr) < 2:
-                put_text("Use open followed by the problem name")
-                return
-            pswd = input("Enter your account password:")
-            user = settings.find_one({"type":"account", "pswd":pswd.strip()})
-            if user is None:
-                put_text("Could not find an account associated with the given password")
-                return
-            put_markdown("**Logged in as `" + user['name'] + "`**")
-            judge.problemInterface(settings, arr[1], user['name'])
-        elif op.lower() == "rank":
-            contest = input("Enter the contest name:")
-            put_markdown(judge.getScoreboard(settings, contest))
-        elif op.lower() == "rem":
-            pswd = input("Enter your account password:")
-            user = settings.find_one({"type":"account", "pswd":pswd.strip()})
-            if user is None:
-                put_text("Could not find an account associated with the given password")
-                return
-            put_markdown("**Logged in as `" + user['name'] + "`**")
-            put_markdown("## Time remaining for joined contests:\n" + judge.remaining(settings, user['name']))
-        else:
-            put_text("Invalid option. Please reload the page and try again.")
+        
 
     except Exception as e:
         put_text("An error occurred. Please make sure your input is valid. Please reload to try again or contact me.")
